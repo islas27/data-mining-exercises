f9ba96ff8daaae5ddbc81bd517b9b6a8764bd816
Jonathan Islas Garcia <islas.27@gmail.com>
2016-09-11 01:51:23 -0600

    Se añade el dataset de practica
    -- Se arreglan errores en las funciones de consola
    -- Se añaden nuevas funciones

--- 1st-term-project/index.js
+++ 1st-term-project/index.js
@@ -40,11 +40,71 @@ let analyzeColumn = function (file, column) {
   stream.pipe(csvStream)
 }
 
+let analyzeMissingValues = function (file, column) {
+  let noValueFound = 0
+  var stream = fs.createReadStream(file)
+  var csvStream = csv(options)
+    .on('data', function (data) {
+      if (data[column] === '') {
+        noValueFound += 1
+      }
+    })
+    .on('end', function () {
+      console.log('Empty records: ' + noValueFound)
+    })
+  stream.pipe(csvStream)
+}
+
+let reorder = function (file, column) {
+  let collection = []
+  var stream = fs.createReadStream(file)
+  var csvStream = csv(options)
+    .on('data', function (data) {
+      collection.push(data)
+    })
+    .on('end', function () {
+      collection.sort(function (a, b) {
+        return a[column] - b[column]
+      })
+      csv
+       .writeToPath('out-r.csv', collection, options)
+       .on('finish', function () {
+         console.log('Done!')
+       })
+    })
+  stream.pipe(csvStream)
+}
+
 let searchColumn = function (file, column, value) {
   var stream = fs.createReadStream(file)
   var csvStream = csv(options)
     .on('data', function (data) {
       if (data[column] === value) {
+        console.log(data)
+      }
+    })
+  stream.pipe(csvStream)
+}
+
+let searchDuplicates = function (file, column) {
+  let collection = []
+  let signatureCollection = []
+  let signature = ''
+  let row = {}
+  let sign = function (key) {
+    if (key !== column) signature = signature + row[key]
+  }
+  var stream = fs.createReadStream(file)
+  var csvStream = csv(options)
+    .on('data', function (data) {
+      signature = ''
+      row = data
+      Object.keys(data).forEach(sign)
+      if (signatureCollection.indexOf(signature) === -1) {
+        collection.push(data)
+        signatureCollection.push(signature)
+      } else {
+        console.log('Found duplicate: ')
         console.log(data)
       }
     })
@@ -112,7 +172,7 @@ let removeValues = function (file, column, value) {
     })
     .on('end', function () {
       csv
-       .writeToPath('out-rd.csv', collection, options)
+       .writeToPath('out-rv.csv', collection, options)
        .on('finish', function () {
          console.log('Removed: ' + removed)
          console.log('Done!')
@@ -121,7 +181,7 @@ let removeValues = function (file, column, value) {
   stream.pipe(csvStream)
 }
 
-let swicthConst = function (file, column, origValue, newValue) {
+let switchConst = function (file, column, origValue, newValue) {
   let collection = []
   let replacedValues = 0
   var stream = fs.createReadStream(file)
@@ -131,7 +191,7 @@ let swicthConst = function (file, column, origValue, newValue) {
         data[column] = newValue
         collection.push(data)
         replacedValues++
-      }
+      } else collection.push(data)
     })
     .on('end', function () {
       csv
@@ -294,7 +354,7 @@ let fillMeanClass = function (file, column, classColumn, type) {
   let fillRow = function (o) {
     if (c[cl].class !== o[classColumn]) cl++
     if (o[column] === '') {
-      o[column] = c[cl].value
+      o[column] = Math.round(c[cl].value)
       modified++
     }
   }
@@ -305,9 +365,10 @@ let fillMeanClass = function (file, column, classColumn, type) {
     })
     .on('end', function () {
       collection.sort(function (a, b) {
-        return a[classColumn] - b[classColumn]
+        if (a[classColumn] < b[classColumn]) return -1
+        if (a[classColumn] > b[classColumn]) return 1
+        return 0
       })
-      console.log(collection)
       if (type === '2') c = getMode(collection)
       else c = getMean(collection)
       collection.forEach(fillRow)
@@ -321,6 +382,102 @@ let fillMeanClass = function (file, column, classColumn, type) {
   stream.pipe(csvStream)
 }
 
+// let fillMeanClass2 = function (file, column, classColumn, type) {
+//   let collection = []
+//   let c
+//   let cl = 0
+//   let modified = 0
+//   let getMode = function (arr) {
+//     var classMapping = []
+//     var currentClass = arr[1][classColumn]
+//     var modeMapping = {}
+//     var greatestFreq = 0
+//     var mode
+//     var appendClass = function (cClass, nClass) {
+//       var classMode = {
+//         class: cClass,
+//         value: mode,
+//         freq: greatestFreq
+//       }
+//       classMapping.push(classMode)
+//       currentClass = nClass
+//       modeMapping = {}
+//       greatestFreq = 0
+//     }
+//     arr.forEach(function (o) {
+//       if (o[column] !== '') {
+//         modeMapping[o[column]] = (modeMapping[o[column]] || 0) + 1
+//         if (greatestFreq < modeMapping[o[column]]) {
+//           greatestFreq = modeMapping[o[column]]
+//           mode = o[column]
+//         }
+//       }
+//       if (currentClass !== o[classColumn]) {
+//         appendClass(currentClass, o[classColumn])
+//       }
+//     })
+//     appendClass(currentClass, null)
+//     console.log(classMapping)
+//     return classMapping
+//   }
+//   let getMean = function (arr) {
+//     let classMapping = []
+//     var currentClass = arr[1][classColumn]
+//     var sum = 0
+//     var count = 0
+//     let appendClass = function (cClass, nClass) {
+//       var classMean = {
+//         class: cClass,
+//         value: sum / count
+//       }
+//       classMapping.push(classMean)
+//       currentClass = nClass
+//     }
+//     arr.forEach(function (o) {
+//       if (o[column] !== '') {
+//         if (currentClass !== o[classColumn]) {
+//           appendClass(currentClass, o[classColumn])
+//           sum = 0
+//           count = 0
+//         }
+//         sum += parseFloat(o[column])
+//         count++
+//       }
+//     })
+//     appendClass(currentClass, null)
+//     console.log(classMapping)
+//     return classMapping
+//   }
+//   let fillRow = function (o) {
+//     if (c[cl].class !== o[classColumn]) cl++
+//     if (o[column] === '') {
+//       o[column] = c[cl].value
+//       modified++
+//     }
+//   }
+//   var stream = fs.createReadStream(file)
+//   var csvStream = csv(options)
+//     .on('data', function (data) {
+//       collection.push(data)
+//     })
+//     .on('end', function () {
+//       collection.sort(function (a, b) {
+//         return a[classColumn] - b[classColumn]
+//       })
+//       console.log(collection)
+//       if (type === '2') c = getMode(collection)
+//       else c = getMean(collection)
+//       collection.forEach(fillRow)
+//       csv
+//        .writeToPath('out-fmc.csv', collection, {headers: true})
+//        .on('finish', function () {
+//          console.log('Modified: ' + modified)
+//          console.log('Done!')
+//        })
+//     })
+//   stream.pipe(csvStream)
+// }
+
 program
 .command('analyze <file>')
 .description('show headers and number of procesable entries')
@@ -332,9 +489,24 @@ program
 .action(analyzeColumn)
 
 program
+.command('analyze-missing <file> <column>')
+.description('Report how many values are missing in a specified column')
+.action(analyzeMissingValues)
+
+program
+.command('reorder <file> <column>')
+.description('Reorder by column')
+.action(reorder)
+
+program
 .command('search-column-value <file> <column> <value>')
-.description('search for entries with a specific valule in the specified column')
+.description('search for entries with a specific value in the specified column')
 .action(searchColumn)
+
+program
+.command('search-duplicates <file> <omitColumn>')
+.description('search duplicates with all values, ommiting a column (like ID)')
+.action(searchDuplicates)
 
 program
 .command('remove-missing <file> <column>')
@@ -354,7 +526,7 @@ program
 program
 .command('switch-const <file> <column> <valueToReplace> <const>')
 .description('fill some column select values with a constant')
-.action(swicthConst)
+.action(switchConst)
 
 program
 .command('fill-const <file> <column> <const>')
@@ -368,8 +540,13 @@ program
 
 program
 .command('fill-mean-class <file> <column> <classColumn> <isNumeric>')
-.description('fill a column missing values with the mean for numeric values or the mode for other types, taking into account the class')
+.description('fill a column missing values with the mean or mode, taking into account the class')
 .action(fillMeanClass)
+
+// program
+// .command('fill-mean-2class <file> <column> <classColumn1> <classColumn2> <isNumeric>')
+// .description('fill a column missing values with the mean or mode, taking into account two classes for calculating')
+// .action(fillMeanClass2)
 
 program.parse(process.argv)
 if (program.args.length === 0) program.help()
